c9a1a74220a040d947f5231c044bd315ca8ea9ac
Garv Jindal <garvjindal2@gmail.com>
2025-01-12 11:19:08 +0530

    updated test.py

--- tracker/tests.py
+++ tracker/tests.py
@@ -1,18 +1,23 @@
 from django.test import TestCase
 from .models import Expense, Category
 from django.contrib.auth.models import User
+from datetime import datetime
+from django.urls import reverse
+
+
 
 class ExpenseTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password ="testpassword")
+        self.client.login(username='testuser', password='testpassword')
         self.category = Category.objects.create(name='Food')
         self.expense = Expense.objects.create(title='Pizza', amount=200, date='2021-07-01', category=self.category, user=self.user)
             
     def test_expense_list(self):
-        response = self.client.get('/expense-list/')
+        response = self.client.get(reverse('expense_list'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'tracker/expense_list.html')
-        self.assertContains(response, 'Pizza')
-        self.assertContains(response, '200')
-        self.assertContains(response, '2021-07-01')
+        formatted_date = datetime(2021, 7, 1).strftime('%B %-d, %Y')
+        self.assertContains(response, formatted_date)
+
+
 
